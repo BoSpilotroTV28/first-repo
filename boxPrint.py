5b8c9efc32272eab939a3a2e9e2d42af3c365948
BoSpilotroTV28 <bospilotro@gmail.com>
2025-02-06 13:23:05 -0700

    made work

--- boxPrint.py
+++ boxPrint.py
@@ -1,8 +1,18 @@
 message = "Hello World in a frame"
 doodadMessage = message.split()
-print("*****")
+thingForSpaces="*********"
+print("*********")
 replay=0
+lengthOfWord=len(thingForSpaces)
+
 for i in doodadMessage:
+  thingyWordLen=len(i)
+  if thingyWordLen<lengthOfWord:
+    numSpaceAdd=lengthOfWord-thingyWordLen
+    rounds=0
+    while rounds < numSpaceAdd:
+      i=i+" "
+      rounds+=1
   print("*"+doodadMessage[replay]+"*")
   replay+=1
-print("*****")
+print("*********")
